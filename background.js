cf0e987b72177506096417647a2538c480213e4f
Grace Lee <jaeeungracelee@gmail.com>
2024-08-01 00:55:59 -0700

    permission errors with mainfest version fixed

--- background.js
+++ background.js
@@ -1,18 +1,1 @@
-chrome.webRequest.onBeforeRequest.addListener(
-    function(details) {
-      const url = new URL(details.url);
-      if (url.hostname.includes('instagram.com') ||
-          (url.hostname.includes('youtube.com') && !isAcademicYouTubeVideo(url))) {
-        return { cancel: true };
-      }
-    },
-    { urls: ["<all_urls>"] },
-    ["blocking"]
-  );
-  
-  function isAcademicYouTubeVideo(url) {
-    const academicKeywords = ["tutorial", "lecture", "education", "course"];
-    return academicKeywords.some(keyword => url.pathname.includes(keyword));
-    // whoops gotta make this better
-  }
-  
+// didn't work so removed it for now
