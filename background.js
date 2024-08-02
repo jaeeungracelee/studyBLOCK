1de3da3afff8e6315683aa9c17417626b40176c1
Grace Lee <jaeeungracelee@gmail.com>
2024-08-01 23:43:51 -0700

    now works without hard-refreshing the page

--- background.js
+++ background.js
@@ -1,4 +1,60 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log('studyBLOCK installed.');
   });
+
+  chrome.runtime.onInstalled.addListener(() => {
+    updateBlockingRules();
+  });
+  
+  chrome.runtime.onStartup.addListener(() => {
+    updateBlockingRules();
+  });
+  
+  function updateBlockingRules() {
+    chrome.declarativeNetRequest.updateDynamicRules({
+      removeRuleIds: [1, 2, 3],
+      addRules: [
+        {
+          "id": 1,
+          "priority": 1,
+          "action": {
+            "type": "block"
+          },
+          "condition": {
+            "urlFilter": "*://*.instagram.com/*",
+            "resourceTypes": ["main_frame"]
+          }
+        },
+        {
+          "id": 2,
+          "priority": 1,
+          "action": {
+            "type": "block"
+          },
+          "condition": {
+            "urlFilter": "*://www.youtube.com/shorts/*",
+            "resourceTypes": ["main_frame"]
+          }
+        },
+        {
+          "id": 3,
+          "priority": 1,
+          "action": {
+            "type": "block"
+          },
+          "condition": {
+            "urlFilter": "*://www.youtube.com/*",
+            "resourceTypes": ["main_frame"],
+            "excludedUrlPatterns": [
+              "*tutorial*",
+              "*lecture*",
+              "*education*",
+              "*course*"
+            ]
+          }
+        }
+      ]
+    });
+    console.log('studyBLOCK rules updated.');
+  }
   
